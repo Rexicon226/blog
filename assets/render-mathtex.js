892916fdc1e25e5e6c8eacae71bf682b0fa57cb8
David Rubin <david@vortan.dev>
2025-05-16 22:42:32 -0700

    elgamal intro

--- assets/render-mathtex.js
+++ assets/render-mathtex.js
@@ -1,3 +1,9 @@
+let inline = document.querySelectorAll("a[href='/$/']");
+for (let i=inline.length-1; i>=0; i--) {
+    let src =  inline[i].textContent;
+    inline[i].outerHTML = temml.renderToString(src, { displayMode: false });
+}
+
 let eqns = document.querySelectorAll("script[type='math/tex']");
 for (let i=eqns.length-1; i>=0; i--) {
     let eqn = eqns[i];
@@ -5,4 +11,3 @@ for (let i=eqns.length-1; i>=0; i--) {
     let d = eqn.closest('p') == null; 
     eqn.outerHTML = temml.renderToString(src, { displayMode: d });
 }
-
